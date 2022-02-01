c021fe2905e1b14df8089d95dd1e93689bb4261e
Dmitry426 <kachkindmitry@gmail.com>
2022-02-01 18:11:24 +0300

    Pyntic models for film,genre,etc

--- async_service/models/film.py
+++ async_service/models/film.py
@@ -1,6 +1,9 @@
+from logging import RootLogger
 import orjson
+from typing import Optional, List
+from async_service.models import genres
 
-
+from pydantic.validators import UUID
 from pydantic import BaseModel
 
 
@@ -8,10 +11,42 @@ def orjson_dumps(v, *, default):
     return orjson.dumps(v, default=default).decode()
 
 
+class PersonFilmWork(BaseModel):
+    id: UUID
+    name: str
+
+
 class Film(BaseModel):
-    id: str
-    title: str
-    description: str
+    id: UUID
+    imdb_rating: float = None
+    genre: List[str]
+    title: str = None
+    description: str = None
+    director: str = None
+    actors_names: Optional[List[str]]
+    writers_names: Optional[List[str]]
+    actors: Optional[List[PersonFilmWork]]
+    writers: Optional[List[PersonFilmWork]]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Person(BaseModel):
+    id: UUID
+    full_name: str
+    role: str
+    film_ids: List[UUID]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class Genre(BaseModel):
+    id: UUID
+    genre: str
 
     class Config:
         json_loads = orjson.loads
