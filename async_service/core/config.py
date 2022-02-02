aa6491e4525b499b7a84169057408070fad95b3e
Brezgunov Dmitriy <d.brezgunov@rambler-co.ru>
2022-02-02 19:24:32 +0300

    linting fixes

--- async_service/core/config.py
+++ async_service/core/config.py
@@ -1,23 +1,19 @@
 import os
 from logging import config as logging_config
 
-from core.logger import LOGGING
 from dotenv import load_dotenv
 
+from core.logger import LOGGING
+
 load_dotenv()
-# Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
-# Название проекта. Используется в Swagger-документации
 PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
 
-# Настройки Redis
 REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = int(os.getenv('REDIS_PORT'))
 
-# Настройки Elasticsearch
 ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT'))
 
-# Корень проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- async_service/models/film.py
+++ async_service/models/film.py
@@ -1,6 +1,4 @@
 import orjson
-
-
 from pydantic import BaseModel
 
 
