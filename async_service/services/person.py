dfa30f0e3466d9c21841ffe66f0fb308d2af0c4d
DmitryK <kachkindmitry@gmail.com>
2022-02-10 20:27:03 +0300

    Cashing for persoN

--- async_service/services/person.py
+++ async_service/services/person.py
@@ -1,9 +1,8 @@
 from functools import lru_cache
 from typing import List, Optional
 
-from aioredis import Redis
 from db.elastic import get_elastic
-from db.redis import get_redis
+
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.exceptions import NotFoundError
 from elasticsearch_dsl import Q, Search
@@ -12,16 +11,18 @@ from models.person import Person
 from models.film import Film
 from pydantic.validators import UUID
 
+from services.cache import RedisBackend, get_redis_backend_service
+
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
 
 class PersonService:
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
+    def __init__(self, redis: RedisBackend, elastic: AsyncElasticsearch):
         self.redis = redis
         self.elastic = elastic
 
     async def get_by_id(self, person_id: UUID) -> Optional[Person]:
-        person = await self._person_from_cache(str(person_id))
+        person = await self.redis.obj_from_cache(str(person_id), Person)
 
         if not person:
             try:
@@ -31,7 +32,7 @@ class PersonService:
 
             if not person:
                 return None
-            await self._put_person_to_cache(person)
+            await self.redis.put_obj_to_cache(person, str(person_id))
 
         return person
 
@@ -39,56 +40,60 @@ class PersonService:
             self, page_size: int, page_number: int,
             query: Optional[str] = None
     ) -> List[Person]:
+        cache_key = f"{page_size}, {page_number},  {query}"
+        result = await self.redis.obj_from_cache(cache_key, Person)
 
-        search = Search(using=self.elastic)
-        if query:
-            search = search.query(Q("multi_match", query=query, fields=['full_name', 'role'], fuzziness='auto'))
+        if not result:
+            search = Search(using=self.elastic)
+            if query:
+                search = search.query(Q("multi_match", query=query, fields=['full_name', 'role'], fuzziness='auto'))
 
-        start = (page_number - 1) * page_size
-        end = start + page_size
-        search = search[start:end]
-        body = search.to_dict()
-        result = await self.elastic.search(index="persons", body=body)
+            start = (page_number - 1) * page_size
+            end = start + page_size
+            search = search[start:end]
+            body = search.to_dict()
+            data = await self.elastic.search(index="persons", body=body)
+            result = [Person(**hit["_source"]) for hit in data["hits"]["hits"]]
 
-        return [Person(**hit["_source"]) for hit in result["hits"]["hits"]]
+            if result:
+                await self.redis.put_obj_to_cache(result, cache_key)
+
+        return result
 
     async def get_films_by_person(self, page_size: int, page_number: int,
                                   person_id: UUID
                                   ) -> List[Film]:
-        search = Search(using=self.elastic)
-        search = search.query(Q("bool",
-                                should=[
-                                    Q("nested", path="directors", query=Q("term", directors__id=person_id)),
-                                    Q("nested", path="actors", query=Q("term", actors__id=person_id)),
-                                    Q("nested", path="writers", query=Q("term", writers__id=person_id)),
-                                ]))
-        start = (page_number - 1) * page_size
-        end = start + page_size
-        search = search[start:end]
-        body = search.to_dict()
-        result = await self.elastic.search(index="movies", body=body)
+        cache_key = f"{page_size}, {page_number},  {person_id}"
+        result = await self.redis.obj_from_cache(cache_key, Film)
 
-        return [Film(**hit["_source"]) for hit in result["hits"]["hits"]]
+        if not result:
+            search = Search(using=self.elastic)
+            search = search.query(Q("bool",
+                                    should=[
+                                        Q("nested", path="directors", query=Q("term", directors__id=person_id)),
+                                        Q("nested", path="actors", query=Q("term", actors__id=person_id)),
+                                        Q("nested", path="writers", query=Q("term", writers__id=person_id)),
+                                    ]))
+            start = (page_number - 1) * page_size
+            end = start + page_size
+            search = search[start:end]
+            body = search.to_dict()
+            data = await self.elastic.search(index="movies", body=body)
+            result = [Film(**hit["_source"]) for hit in data["hits"]["hits"]]
+            if result:
+                await self.redis.put_obj_to_cache(result, cache_key)
+
+        return result
 
     async def _get_person_from_elastic(self, person_id: UUID) -> Optional[Person]:
         doc = await self.elastic.get("persons", person_id)
         return Person(**doc["_source"])
 
-    async def _person_from_cache(self, person_id: str) -> Optional[Person]:
-        data = await self.redis.get(person_id)
-        if not data:
-            return None
-
-        person = Person.parse_raw(data)
-        return person
-
-    async def _put_person_to_cache(self, person: Person):
-        await self.redis.set(str(person.id), person.json(), expire=FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
 @lru_cache()
 def get_person_service(
-        redis: Redis = Depends(get_redis),
+        redis: RedisBackend = Depends(get_redis_backend_service),
         elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> PersonService:
     return PersonService(redis, elastic)
