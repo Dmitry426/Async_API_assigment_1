b2346ee7a25649d902358d6e90536b8add4a5e1b
DmitryK <kachkindmitry@gmail.com>
2022-02-12 17:23:28 +0300

    Code  refactoring

--- async_service/models/person.py
+++ async_service/models/person.py
@@ -1,20 +1,12 @@
 from typing import List
 
-import orjson
-from pydantic import BaseModel
 from pydantic.validators import UUID
 
-
-def orjson_dumps(v, *, default):
-    return orjson.dumps(v, default=default).decode()
+from .film import JsonConfig
 
 
-class Person(BaseModel):
+class Person(JsonConfig):
     id: UUID
     full_name: str
     role: List[str]
     film_works: List[UUID]
-
-    class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
