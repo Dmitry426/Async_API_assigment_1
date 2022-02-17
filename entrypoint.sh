0a90c78e8a71a86d5ce8b08c14f5162b61edfd3b
DmitryK <kachkindmitry@gmail.com>
2022-02-17 20:41:55 +0300

    Entrypoint script waits wor es and redis to lauch

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-for i in {30..0}; do
-    if curl elasticsearch:9200; then
-     echo "Es started "
-            break;
-    fi
-    sleep 2
-
---
+++ tests/functional/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+es_host="es01_test"
+es_port="9200"
+redis_host="redis_server_test"
+redis_port="6379"
+cmd="$@"
+
+>&2 echo "!!!!!!!! Check elastic status:available !!!!!!!!"
+
+until curl http://"$es_host":"$es_port"; do
+  >&2 echo "Elastic container is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Es Container is running ok"
+
+>&2 echo "!!!!!!!! Check redis status:available !!!!!!!!"
+
+until redis-cli -h "$redis_host" -p "$redis_port" ; do
+  >&2 echo "Redis  container is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Redis Container is running ok"
+
+exec  echo "Here is main test command running"
