f59b826339827d9c4b3c23c484c5181ad30048ab
DmitryK <kachkindmitry@gmail.com>
2022-02-18 23:48:39 +0300

    Data_path , base url fields

--- tests/functional/core/settings.py
+++ tests/functional/core/settings.py
@@ -2,7 +2,7 @@ from pydantic.env_settings import BaseSettings
 from pydantic.fields import Field
 
 
-class ESSettings(BaseSettings):
+class EsSettings(BaseSettings):
     host: str = Field('127.0.0.1', env="ELASTIC_HOST")
     port: str = Field('5432', env="ELASTIC_PORT")
 
@@ -18,3 +18,17 @@ class RedisSettings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+
+
+class UvicornURL(BaseSettings):
+    host: str = Field('127.0.0.1', env="UVICORN_HOST")
+    port: str = Field('8000', env="UVICORN_PORT")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+class BasePath(BaseSettings):
+    index_path: str = Field('./testdata/index_schemas', env="INDEX_PATH")
+    data_path: str = Field('./testdata', env="DATA_PATH")
