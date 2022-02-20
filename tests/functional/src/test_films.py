56e44009f5462b402086f0f11f75bff4a6bb63a9
DmitryK <kachkindmitry@gmail.com>
2022-02-20 23:37:23 +0300

    Film by  id query test

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -2,7 +2,11 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_search_detailed(make_get_request):
-    response = await make_get_request('/search', {'search': 'Star Wars'})
-    assert response.status == 404
-    assert len(response.body) == 1
+async def test_load_by_uuid(make_get_request, load_test_data, create_es_index):
+    await create_es_index('movies')
+    await load_test_data('film')
+    response = await make_get_request('/film', '00e2e781-7af9-4f82-b4e9-14a488a3e184')
+    assert response.status == 200
+    assert len(response.body) == 5
+    assert response.body[0]['imdb_rating'] == 6.5
+    assert response.body[0]['title'] == "Star Slammer"
