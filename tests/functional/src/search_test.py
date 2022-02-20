0e6c78adeb521ff1eaca1c5d12c191187d13ea83
DmitryK <kachkindmitry@gmail.com>
2022-02-20 23:38:44 +0300

    Search by keyword test

--- tests/functional/src/search_test.py
+++ tests/functional/src/search_test.py
@@ -1,9 +1,8 @@
 import pytest
-import warnings
 
 
 @pytest.mark.asyncio
-async def test_search_detailed(make_get_request, load_test_data, create_es_index, remove_es_data):
+async def test_search_detailed(make_get_request, load_test_data, create_es_index):
     await create_es_index('movies')
     await load_test_data('film')
     response = await make_get_request('/film', {'search': 'star '})
@@ -12,14 +11,6 @@ async def test_search_detailed(make_get_request, load_test_data, create_es_index
 
 
 
-
-@pytest.mark.asyncio
-async def test_search_detailed2(make_get_request, load_test_data, create_es_index, remove_es_data):
-    await create_es_index('movies')
-    await load_test_data('film')
-    response = await make_get_request('/film', {'search': 'star '})
-    assert response.status == 200
-    assert len(response.body) == 5
 
 
 
