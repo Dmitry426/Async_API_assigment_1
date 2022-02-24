b6c476410a0d2a7c139be8ca01a687c6762dc9bb
DmitryK <kachkindmitry@gmail.com>
2022-02-24 23:48:29 +0300

    Tests for a film endpoint

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 from http import HTTPStatus
 
 import pytest
@@ -24,7 +25,7 @@ async def test_query_with_params(make_get_request, es_client, redis_client):
     assert response.status == HTTPStatus.OK
     logger.debug('Response status : %s', response.status)
     assert len(response.body) == page_size
-    logger.info("Response length : %s", len(response.body))
+    logger.info("Response page size : %s", len(response.body))
 
 
 @pytest.mark.asyncio
@@ -32,15 +33,15 @@ async def test_query_with_params_wrong(make_get_request, es_client, redis_client
     response = await make_get_request(
         method='/film',
         params={'pagessdize': "sdaD"})
-    assert response.status == HTTPStatus.ok
+    assert response.status == HTTPStatus.OK
     logger.debug('Response status : %s', response.status)
-    assert len(response.body)
+    assert len(response.body) == 50
     logger.info("Response length : %s", len(response.body))
 
 
 @pytest.mark.asyncio
 async def test_search_detailed(make_get_request, es_client, redis_client):
-    response = await make_get_request('/film', {'search': 'star '})
+    response = await make_get_request('/film/search', {'query': 'star '})
     assert response.status == HTTPStatus.OK
     logger.debug('Response status : %s', response.status)
     assert len(response.body) == 50
@@ -48,11 +49,12 @@ async def test_search_detailed(make_get_request, es_client, redis_client):
 
 
 @pytest.mark.asyncio
-async def test_search_no_query(make_get_request, es_client, redis_client):
-    response = await make_get_request('/film', {'search': ''})
+async def test_search_false_query(make_get_request, es_client, redis_client):
+    search_query: str = "sfsjfsjf "
+    response = await make_get_request('/film/search', params={'query': f'{search_query}'})
     assert response.status == HTTPStatus.OK
     logger.debug('Response status : %s', response.status)
-    assert len(response.body) == 50
+    assert len(response.body) == 0
     logger.info("Response length : %s", len(response.body))
 
 
@@ -64,7 +66,7 @@ async def test_sort_success(make_get_request, es_client, redis_client):
     assert response.status == HTTPStatus.OK
     logger.debug('Response status : %s', response.status)
     assert response.body[0]["imdb_rating"] == 9.2
-    logger.info("Response length : %s", response.body[0]["imdb_rating"])
+    logger.info("Response Imdb rating max : %s", response.body[0]["imdb_rating"])
 
 
 @pytest.mark.asyncio
@@ -72,7 +74,7 @@ async def test_sort_by_genre_success(make_get_request, es_client, redis_client):
     genre_id = "55c723c1-6d90-4a04-a44b-e9792040251a"
     response = await make_get_request(
         method='/film',
-        params={'page[size]': 50, 'page[number]': 1})
+        params={'page[size]': 10, 'page[number]': 1, "filter[genre]":f'{genre_id}'})
     assert response.status == HTTPStatus.OK
     logger.debug('Response status : %s', response.status)
     assert response.body[0]
@@ -83,6 +85,17 @@ async def test_sort_by_genre_success(make_get_request, es_client, redis_client):
 async def test_load_by_uuid(make_get_request, es_client):
     film_id = 'ec0399e9-abcd-46b7-94d9-f1d8a6c515e9'
     response = await make_get_request(f'/film/{film_id}')
-    assert response.status == 200
+    assert response.status == HTTPStatus.OK
     logger.debug('Response status : %s', response.status)
     assert response.body['title'] == "You're a Star"
+    logger.info('Response title: %s', response.body['title'])
+
+
+@pytest.mark.asyncio
+async def test_by_fake_uuid(make_get_request, es_client):
+    film_id = uuid.uuid4()
+    response = await make_get_request(f'/film/{film_id}')
+    assert response.status == HTTPStatus.NOT_FOUND
+    logger.debug('Response status : %s', response.status)
+    assert len(response.body) == 0
+    logger.info()
