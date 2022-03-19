e2221cd98ec8bebcab4eb8e4bc4ed2936d9a783a
DmitryK <kachkindmitry@gmail.com>
2022-03-19 23:56:14 +0300

    Updataed redis connection

--- async_service/db/redis.py
+++ async_service/db/redis.py
@@ -1,9 +1,17 @@
+from functools import lru_cache
 from typing import Optional
 
+import aioredis
 from aioredis import Redis
+from async_service.core import config
 
 redis: Optional[Redis] = None
 
 
-async def get_redis() -> Redis:
-    return redis
+@lru_cache
+def get_redis() -> Redis:
+    return aioredis.from_url(
+        f"redis://{config.REDIS_HOST}:{config.REDIS_PORT}/{config.REDIS_DB}",
+        encoding="utf8",
+        decode_responses=True,
+    )
