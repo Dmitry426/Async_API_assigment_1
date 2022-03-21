04bafd350024bb04686ca57768c0636f3b77a4da
DmitryK <kachkindmitry@gmail.com>
2022-03-22 00:01:48 +0300

    New wait for ping fixture

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,39 +1,36 @@
 import asyncio
 import json
 import logging
-from dataclasses import dataclass
-from typing import Any, Dict
+from typing import Any, Dict, Union, Optional
 
 import aiofiles
-import aiohttp
 import aioredis
+import backoff
 import elasticsearch
 import pytest_asyncio
-from dotenv import load_dotenv
+from aiohttp import ClientSession
+from aioredis import Redis
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_bulk
-from multidict import CIMultiDictProxy
+from pydantic import BaseModel
 
-from .core.settings import EsSettings, RedisSettings, TestSettings, UvicornURL
+from .settings import TestSettings
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 es_logger = elasticsearch.logger
 es_logger.setLevel(elasticsearch.logging.WARNING)
 
-load_dotenv()
-ES_SETTINGS = EsSettings().dict()
-REDIS_SETTINGS = RedisSettings().dict()
-SERVICE_URL = UvicornURL().dict()
 
-config = TestSettings.parse_file("./test_config.json")
-
-
-@dataclass
-class HTTPResponse:
-    body: dict
-    headers: CIMultiDictProxy[str]
+class HTTPResponse(BaseModel):
+    body: Any
+    headers: Dict[str, Any]
     status: int
+
+
+@pytest_asyncio.fixture(name="settings", scope="session")
+def settings_fixture() -> TestSettings:
+    return TestSettings()
 
 
 def generate_data(index: str, data: Dict) -> Dict:
@@ -41,9 +38,9 @@ def generate_data(index: str, data: Dict) -> Dict:
         yield dict(_index=index, _id=item["id"], _source=item)
 
 
-async def remove_es_indexes(elastic_client):
+async def remove_es_indexes(elastic_client, settings: TestSettings):
     remove_index = []
-    for index in config.es_indexes:
+    for index in settings.es_indexes:
         if await elastic_client.indices.exists(index=index):
             remove_index.append(index)
     await elastic_client.indices.delete(index=remove_index)
@@ -56,39 +53,40 @@ async def load_json_data(file_path: str, index: str) -> Dict[str, Any]:
     return result
 
 
-async def load_test_data(elastic_client: AsyncElasticsearch, index: str):
-    result = await load_json_data(file_path=config.data_path, index=index)
+async def load_test_data(elastic_client: AsyncElasticsearch, index: str, settings: TestSettings):
+    result = await load_json_data(file_path=settings.data_path, index=index)
     await async_bulk(elastic_client, generate_data(index, result))
-    await asyncio.sleep(1)
 
 
-async def create_es_index(elastic_client: AsyncElasticsearch, index: str):
+async def create_es_index(elastic_client: AsyncElasticsearch, index: str, settings: TestSettings):
     if not await elastic_client.indices.exists(index=index):
-        result = await load_json_data(file_path=config.index_path, index=index)
+        result = await load_json_data(file_path=settings.index_path, index=index)
         await elastic_client.indices.create(index=index, body=result)
-    await asyncio.sleep(1)
+    await asyncio.sleep(0.5)
 
 
 @pytest_asyncio.fixture(scope="session", name="es_client")
-async def es_client() -> AsyncElasticsearch:
+async def es_client(settings: TestSettings) -> AsyncElasticsearch:
     client = AsyncElasticsearch(
-        hosts=f'http://{ES_SETTINGS["host"]}:{ES_SETTINGS["port"]}'
+        hosts=f'http://{settings.es_settings.host}:{settings.es_settings.port}'
     )
-    for index in config.es_indexes:
-        await create_es_index(elastic_client=client, index=index)
-        await load_test_data(elastic_client=client, index=index)
+    await wait_for_ping(client=client,settings=settings)
+    for index in settings.es_indexes:
+        await create_es_index(elastic_client=client, index=index, settings=settings)
+        await load_test_data(elastic_client=client, index=index, settings=settings)
     yield client
-    # await remove_es_indexes(elastic_client=client)
+    await remove_es_indexes(elastic_client=client, settings=settings)
     await client.close()
 
 
 @pytest_asyncio.fixture(name="redis_client", scope="session")
-async def redis_client_fixture() -> aioredis:
+async def redis_client_fixture(settings: TestSettings) -> aioredis:
     redis = aioredis.from_url(
-        f"redis://{REDIS_SETTINGS['host']}:{REDIS_SETTINGS['port']}",
+        f"redis://{settings.redis_settings.host}:{settings.redis_settings.port}/{settings.redis_settings.db}",
         encoding="utf8",
         decode_responses=True,
     )
+    await wait_for_ping(client=redis, settings=settings)
     yield redis
     await redis.flushall()
     await redis.close()
@@ -101,22 +99,61 @@ def event_loop():
     loop.close()
 
 
-@pytest_asyncio.fixture(scope="session")
-async def session():
-    session_aio = aiohttp.ClientSession()
-    yield session_aio
-    await session_aio.close()
+@pytest_asyncio.fixture(name="http_client", scope="session")
+async def http_client_fixture(settings, redis_client, es_client) -> ClientSession:
+    """Represents HTTP client fixture.
+
+    Add dependency fixtures `postgres_client` and `redis_client` to
+    check they are ready to work.
+    """
+    async with ClientSession(
+            base_url=f"http://{settings.url_settings.host}:{settings.url_settings.port}") as session:
+        yield session
 
 
-@pytest_asyncio.fixture(scope="function")
-def make_get_request(session):
-    async def inner(method: str, params: dict = None) -> HTTPResponse:
+async def wait_for_ping(client: Union[Redis, AsyncElasticsearch], settings: TestSettings):
+    """Wait for service client to answer"""
+    client_name = type(client).__name__
+
+    @backoff.on_exception(
+        wait_gen=backoff.expo,
+        exception=(RuntimeError, ConnectionError, TimeoutError),
+        max_time=settings.ping_timeout,
+    )
+    async def _ping(inner_client):
+        result = await inner_client.ping()
+        if not result:
+            raise RuntimeError(f"{client_name} still not ready...")
+
+    return await _ping(client)
+
+
+@pytest_asyncio.fixture(name="make_get_request", scope="session")
+def make_get_request(http_client: ClientSession):
+    """Make HTTP-request"""
+
+    async def inner(
+            url: str,
+            params: Optional[Dict[str, Any]] = None,
+            jwt: Optional[str] = None,
+    ) -> HTTPResponse:
         params = params or {}
-        url = f'http://{SERVICE_URL["host"]}:{SERVICE_URL["port"]}{config.api_path}{method}'
-        async with session.get(url, params=params) as response:
+        headers = {}
+
+        if jwt:
+            headers = {"Authorization": "Bearer {}".format(jwt)}
+
+        logger.debug("URL: %s", url)
+
+        async with http_client.get(
+                url, params=params, headers=headers
+        ) as response:
+            body = await response.json()
+            logger.warning("Response: %s", body)
+
             return HTTPResponse(
-                body=await response.json(),
-                headers=response.headers,
+                body=body,
+                headers=dict(response.headers),
                 status=response.status,
             )
 
