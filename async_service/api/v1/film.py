fe92a38de97a99fcdfb7ae87d918536d7a252523
DmitryK <kachkindmitry@gmail.com>
2022-03-24 19:35:02 +0300

    Verify token

--- async_service/api/v1/film.py
+++ async_service/api/v1/film.py
@@ -1,31 +1,27 @@
+import logging
 from http import HTTPStatus
 from typing import List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, Header, Security
+from fastapi.security import HTTPBearer
 from fastapi_cache.decorator import cache
-from pydantic import BaseModel
+
 from pydantic.validators import UUID
 
-from async_service.core.config import API_CACHE_TTL
-from async_service.models.film import PersonGenreFilm
-from async_service.models.genre import Genre
-from async_service.services.base_service import FilmService, get_film_service
+from async_service.core.config import RedisSettings, JwtSettings
+from async_service.serializers.auth import TokenData
+
+from async_service.serializers.film import FilmList, FilmDetail
+
+from async_service.services.base_service import FilmService, get_film_service, AuthService
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 router = APIRouter()
+redis_settings = RedisSettings()
+jwt = JwtSettings()
 
-
-class FilmList(BaseModel):
-    id: UUID
-    title: str
-    rating: float = None
-
-
-class FilmDetail(FilmList):
-    description: Optional[str]
-    genres: Optional[List[Genre]]
-    actors: Optional[List[PersonGenreFilm]]
-    writers: Optional[List[PersonGenreFilm]]
-    directors: Optional[List[PersonGenreFilm]]
+security = HTTPBearer(auto_error=False)
+auth = AuthService()
 
 
 @router.get(
@@ -37,19 +33,28 @@ class FilmDetail(FilmList):
     Returns paginated list of films sorted by search score.
     """,
 )
-@cache(expire=API_CACHE_TTL)
+@cache(expire=redis_settings.cache_ttl)
 async def film_search(
-    query: str,
-    film_service: FilmService = Depends(get_film_service),
-    page_size: int = Query(50, alias="page[size]"),
-    page_number: int = Query(1, alias="page[number]"),
-    sort: str = Query("rating"),
+        query: str,
+        film_service: FilmService = Depends(get_film_service),
+        page_size: int = Query(50, alias="page[size]"),
+        page_number: int = Query(1, alias="page[number]"),
+        sort: str = Query("rating"),
+        credentials: Optional[HTTPAuthorizationCredentials] = Security(security)
 ) -> List[FilmList]:
+    token = credentials
+    roles: TokenData
+    if token:
+        result = auth.decode_token(token=token.credentials)
+        roles = TokenData(roles=result["roles"])
+    if not token:
+        roles = TokenData(roles=["visitor"])
+
     films = await film_service.get_list_search(
-        page_size=page_size, page_number=page_number, query=query, sort=sort
+        page_size=page_size, page_number=page_number, query=query, sort=sort, roles=roles
     )
     return [
-        FilmList(**film.dict(include={"id", "title", "imdb_rating"})) for film in films
+        FilmList(**film.dict(include={"id", "title", "rating"})) for film in films
     ]
 
 
@@ -59,15 +64,15 @@ async def film_search(
     name="Film by ID",
     description="Returns specific film by its UUID.",
 )
-@cache(expire=API_CACHE_TTL)
+@cache(expire=redis_settings.cache_ttl)
 async def film_details(
-    film_id: UUID, film_service: FilmService = Depends(get_film_service)
+        film_id: UUID, film_service: FilmService = Depends(get_film_service)
 ) -> FilmDetail:
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
 
-    return FilmDetail(**film.dict(exclude={"actors_names", "writers_names"}))
+    return FilmDetail(**film.dict(exclude={"actors_names", "writers_names", "role"}))
 
 
 @router.get(
@@ -76,16 +81,25 @@ async def film_details(
     name="Films list",
     description="Returns paginated list of films sorted and filtered by corresponding params.",
 )
-@cache(expire=API_CACHE_TTL)
+@cache(expire=redis_settings.cache_ttl)
 async def film_list(
-    film_service: FilmService = Depends(get_film_service),
-    sort: str = Query("rating"),
-    page_size: int = Query(50, alias="page[size]"),
-    page_number: int = Query(1, alias="page[number]"),
-    filter_genre: Optional[UUID] = Query(None, alias="filter[genre]"),
+        film_service: FilmService = Depends(get_film_service),
+        sort: str = Query("rating"),
+        page_size: int = Query(50, alias="page[size]"),
+        page_number: int = Query(1, alias="page[number]"),
+        filter_genre: Optional[UUID] = Query(None, alias="filter[genre]"),
+        credentials: Optional[HTTPAuthorizationCredentials] = Security(security)
 ) -> List[FilmList]:
+    token = credentials
+    roles: TokenData
+    if token:
+        result = auth.decode_token(token=token.credentials)
+        roles = TokenData(roles=result["roles"])
+    if not token:
+        roles = TokenData(roles=["visitor"])
+
     films = await film_service.get_list_filter_by_id(
-        page_size=page_size, page_number=page_number, sort=sort, genre_id=filter_genre
+        page_size=page_size, page_number=page_number, sort=sort, genre_id=filter_genre, roles=roles
     )
 
     return [FilmList(**film.dict(include={"id", "title", "rating"})) for film in films]
