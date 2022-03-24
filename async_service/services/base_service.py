3cb556d09a9d844bad33a0d5d61ff0c8723533c5
DmitryK <kachkindmitry@gmail.com>
2022-03-24 19:37:29 +0300

    Jwt service interface

--- async_service/services/base_service.py
+++ async_service/services/base_service.py
@@ -1,13 +1,20 @@
 from abc import ABC
+from functools import lru_cache
+from urllib.request import Request
 
 from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
 
+
+from async_service.core.config import JwtSettings
 from async_service.db.elastic import get_elastic
 from async_service.models.film import Film
 from async_service.models.genre import Genre
 from async_service.models.person import Person
 from async_service.services.es_service import EsService
+from async_service.services.jwt_utils import Auth
+
+jwt = JwtSettings()
 
 
 class FilmService(EsService, ABC):
@@ -15,8 +22,9 @@ class FilmService(EsService, ABC):
     response_model = Film
 
 
+@lru_cache
 def get_film_service(
-    elastic: AsyncElasticsearch = Depends(get_elastic),
+        elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> FilmService:
     return FilmService(elastic)
 
@@ -26,8 +34,9 @@ class GenreService(EsService, ABC):
     response_model = Genre
 
 
+@lru_cache
 def get_genre_service(
-    elastic: AsyncElasticsearch = Depends(get_elastic),
+        elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> GenreService:
     return GenreService(elastic)
 
@@ -37,7 +46,15 @@ class PersonService(EsService, ABC):
     response_model = Person
 
 
+@lru_cache
 def get_person_service(
-    elastic: AsyncElasticsearch = Depends(get_elastic),
+        elastic: AsyncElasticsearch = Depends(get_elastic),
 ) -> PersonService:
     return PersonService(elastic)
+
+
+class AuthService(Auth, ABC):
+    secret_key = jwt.secret_key
+    algorithm = jwt.algorithm
+
+
